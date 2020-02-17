a0f3ba1ba8c132891d2691aa598f62c315289ade
root <root@mars.macrotemporal.com>
2020-02-16 16:18:58 -0800

    add code for get_all_xfer_info, cancelling transfers and getting bandwidth info

--- rest_calls.py
+++ rest_calls.py
@@ -1,10 +1,17 @@
 import requests
 
-def get_xfer_info(options, sslverify, username, pw, host):
+def get_all_xfer_info(options, sslverify, username, pw, host):
     return requests.get(_url(host,'/ops/transfers'), verify=sslverify, params=options, auth=(username, pw))
 
-def get_bandwidth_usage(options, sslverify, username, pw, host):
+def get_xfer_info(options, transferid, sslverify, username, pw, host):
+    return requests.get(_url(host,'/ops/transfers/' + transferid), verify=sslverify, params=options, auth=(username, pw))
 
+# requires activity_bandwidth_logging to be enabled on transfer server
+def get_bandwidth_usage(options, sslverify, username, pw, host):
+    return requests.get(_url(host,'/ops/transfers/bandwidth'), verify=sslverify, params=options, auth=(username, pw))
+
+def cancel_transfer(options, transferid, sslverify, username, pw, host):
+    return requests.request("CANCEL", _url(host,'/ops/transfers/' + transferid), verify=sslverify, params=options, auth=(username, pw))
 
 def _url(host, path):
     return 'https://' + host + ':9092' + path
